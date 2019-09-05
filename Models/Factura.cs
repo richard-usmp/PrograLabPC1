d0ce0077816f5e4030091b6deb0b7bc5c5a91aca
Ricardo Leon <ricardo_leon1@usmp.pe>
2019-09-05 18:46:59 -0500

    Lsito

--- Models/Factura.cs
+++ Models/Factura.cs
@@ -10,6 +10,7 @@ namespace PrograLabPC1.Models
         public String Name {get; set;}
         public String Producto {get; set;}
         public int Cantidad {get; set;}
+        
         [DataType(DataType.Date)]
         public DateTime Date {get; set;}
         public int Subtotal{get; set;}
